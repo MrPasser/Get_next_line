c149a68bd820dce21cb436e3ffb833afa1c34ed1
Svyatoslav Krasin <skrasin@e1z2r4p9.42.us.org>
2019-11-13 15:07:31 -0800

    last version

--- get_next_line.c
+++ get_next_line.c
@@ -6,91 +6,81 @@
 /*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 19:53:15 by skrasin           #+#    #+#             */
-/*   Updated: 2019/11/04 15:04:22 by skrasin          ###   ########.fr       */
+/*   Updated: 2019/11/12 21:03:25 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define CONT (store->content)
-#define P_NL (ft_strchr(CONT, '\n'))
-#include <sys/stat.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
 
 static t_list	*ft_lstsearch(size_t content_size)
 {
-	static t_list	*store;
-	t_list			*tmp;
+	static t_list	*store = NULL;
+	t_list			*node;
+	char			*tmp;
 
-	tmp = store;
-	while (tmp)
+	node = store;
+	while (node)
 	{
-		if (tmp->content_size == content_size)
-			return (tmp);
-		tmp = tmp->next;
+		if (node->content_size == content_size)
+			return (node);
+		node = node->next;
 	}
-	ft_lstadd(&tmp, ft_lstnew(ft_strnew(BUFF_SIZE), BUFF_SIZE));
-	tmp->content_size = content_size;
-	store = tmp;
-	return (tmp);
+	tmp = ft_strnew(BUFF_SIZE);
+	ft_lstadd(&store, ft_lstnew(tmp, BUFF_SIZE + 1));
+	free(tmp);
+	if (store)
+	{
+		store->content_size = content_size;
+		return (store);
+	}
+	else
+		return (NULL);
 }
 
-static char		*ft_strextend(char **dst, char const *src, size_t len)
-{
-	char *tmp;
-
-	tmp = ft_strjoin(*dst, ft_strsub(src, 0, len));
-	if (*dst)
-		free(*dst);
-	*dst = tmp;
-	return (*dst);
-}
-
-static void		ft_strcut(char **str, char *sep)
+static char		*ft_strextend(char **dst, char const *src)
 {
 	char	*tmp;
-	size_t	i;
+	size_t	n1;
 
-	i = sep - *str;
-	if (*sep != '\0')
-		i++;
-	tmp = ft_strdup(*str + i);
-	free(*str);
-	*str = tmp;
+	if (dst && *dst && src)
+	{
+		tmp = *dst;
+		n1 = ft_strlen(*dst);
+		if ((*dst = ft_memalloc(n1 + ft_strlen(src) + 1)))
+		{
+			*dst = ft_memcpy(*dst, tmp, n1 + 1);
+			while (*src)
+				*(*dst + n1++) = *src++;
+			*(*dst + n1) = '\0';
+		}
+		free(tmp);
+	}
+	return (*dst);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	ssize_t			len;
-	t_list			*store;
+	t_list			*node;
+	char			buf[BUFF_SIZE + 1];
+	char			*nl;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || read(fd, 0, 0) || !(node = ft_lstsearch(fd)))
 		return (-1);
-	free(*line);
-	*line = ft_strnew(0);
-	store = ft_lstsearch(fd);
-	while ((len = read(fd, CONT, BUFF_SIZE)) > 0)
+	while (!(nl = ft_strchr(CONT, '\n')))
 	{
-		if (P_NL)
+		if ((len = read(fd, buf, BUFF_SIZE)) <= 0)
 			break ;
-		*line = ft_strextend(line, CONT, len);
-		ft_strcut((char **)&(CONT), (CONT) + ft_strlen(CONT));
+		buf[len] = '\0';
+		ft_strextend((char **)&CONT, buf);
 	}
-	if (len < 0)
-		return (-1);
-	if (P_NL - (char *)(CONT) > -1 && *(char *)CONT != '\0')
-	{
-		*line = ft_strextend(line, CONT, P_NL - (char *)(CONT));
-		ft_strcut((char **)&(CONT), P_NL);
-		return (1);
-	}
-	*line = ft_strextend(line, CONT, ft_strlen(CONT));
-	ft_strcut((char **)&(CONT), (CONT) + ft_strlen(CONT));
-	if (len == 0 && *(char *)CONT == '\0' && **line == '\0')
-		return (0);
-	return (1);
+	*line = nl ? ft_strsub(CONT, 0, nl - (char *)CONT) : ft_strdup(CONT);
+	*(char *)CONT = '\0';
+	nl ? ft_strextend((char **)&CONT, nl + 1) :
+									ft_strextend((char **)&CONT, CONT);
+	return ((**line == '\0' && !len) ? 0 : 1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 23:21:14 by skrasin           #+#    #+#             */
-/*   Updated: 2019/11/04 14:09:36 by skrasin          ###   ########.fr       */
+/*   Updated: 2019/11/12 19:57:51 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include <string.h>
 # include <unistd.h>
 # include "./libft/libft.h"
-# define BUFF_SIZE (4096)
+# define BUFF_SIZE (1)
+# define CONT (node->content)
 
 int		get_next_line(const int fd, char **line);
 
